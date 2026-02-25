2ba01c1e329da050ff5613ecb0fb9117205eac03
Eddie <eddieshiung@gamail.com>
2026-02-25 15:16:38 +0800

    Initial commit

--- app/src/main/java/com/rootilabs/wmeCardiac/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/rootilabs/wmeCardiac/ui/profile/ProfileScreen.kt
@@ -1,22 +1,14 @@
 package com.rootilabs.wmeCardiac.ui.profile
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -24,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.Image
 import com.rootilabs.wmeCardiac.R
 import com.rootilabs.wmeCardiac.di.ServiceLocator
 import com.rootilabs.wmeCardiac.ui.theme.TagGoGreen
